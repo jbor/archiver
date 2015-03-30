e85f6e1a4a7e29ce6177b4d6adfacddee21f4b9e
jbor <j.a.borst@gmail.com>
2015-03-30 15:50:48 +0200

    about

--- ArchiveUI/FormAbout.Designer.cs
+++ ArchiveUI/FormAbout.Designer.cs
@@ -44,12 +44,16 @@
             // 
             // richTextBox1
             // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
             this.richTextBox1.Size = new System.Drawing.Size(260, 139);
             this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "vhmbnmmbn\nl\';l\';l\'\n";
+            this.richTextBox1.Text = "This is just a simple study project to provide a graphical interface for configur" +
+    "ation of the archiver.ps1 Powershell script.\n\nhttp://github.com/jbor/archiver\n\n\n" +
+    "";
             // 
             // FormAbout
             // 
@@ -59,7 +63,7 @@
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.buttonClose);
             this.Name = "FormAbout";
-            this.Text = "FormAbout";
+            this.Text = "About";
             this.ResumeLayout(false);
 
         }
