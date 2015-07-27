7301602465d0a82ba5d34f58e31056122e5d3fc6
jbor <j.a.borst@gmail.com>
2015-07-27 15:57:19 +0200

    XML

--- ArchiveUI/Form1.cs
+++ ArchiveUI/Form1.cs
@@ -7,22 +7,237 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.IO;
 
 namespace ArchiveUI
 {
 
     public partial class Form1 : Form
     {
-
+        string path = "archive.xml";
         Boolean Enable_edit;
               
         public Form1()
         {
             InitializeComponent();
             counter = 0;
-            Parameters = GetXMLData();
+            //Parameters = GetXMLData();
+            Parameters = GetXMLData2();
             FillData();
         }
+
+
+            int counter;
+            //Parameter[] Parameters;
+            List<Parameter> Parameters;
+
+            public class Parameter
+            {
+                //Eigenlijk moet zo'n beetje alles er als een string uitkomen, om in de form velden te worden geprint, 
+                //maar voor de validatie is het wel netjes om de items de normale waardes te geven.
+                private string interfacename;
+                private string procesdir;
+                private string archivedir;
+                private int timespan;
+                private bool recursive;
+                private int retention;
+                private string include;
+                private string exclude;
+                private string comments;
+
+                public string InterfaceName { 
+                    get 
+                    {
+                        return interfacename;  
+                    }
+                    set
+                    {
+                        interfacename = value;
+                    }
+                }
+
+                public string Comments
+                {
+                    get { return comments; }
+                    set { comments = value; }
+                }
+
+                public string Exclude
+                {
+                    get { return exclude; }
+                    set { exclude = value; }
+                }
+
+                public string Include
+                {
+                    get { return include; }
+                    set { include = value; }
+                }
+                
+                public string Retention
+                {
+                    get { return retention.ToString(); }
+                    set { retention = Convert.ToInt32(value); }
+                }
+
+
+                public bool Recursive
+                {
+                    get { return recursive; }
+                    set { recursive = value; }
+                }                
+
+
+                public int Timespan
+                {
+                    get 
+                    {
+                        return timespan;                    
+                    }
+                    set
+                    {
+                       timespan = value;
+                    
+                    }
+                }
+                
+                public string ArchiveDir
+                {
+                    get { return archivedir; }
+                    set { archivedir = value; }
+                }
+                
+                public string ProcesDir
+                {
+                    get { return procesdir; }
+                    set { procesdir = value; }
+                }
+
+                //Constructor met converter voor Timespan
+                public Parameter(string Timespan)
+                {
+                    switch (Timespan)
+                    {
+                        case "D":
+                            timespan = 0;
+                            break;
+                        case "W":
+                            timespan = 1;
+                            break;
+                        case "M":
+                            timespan = 2;
+                            break;
+                        case "Y":
+                            timespan = 3;
+                            break;
+                        default:
+                            timespan = Convert.ToInt32(Timespan);
+                            break;
+                    }
+                }
+
+            }
+
+            private List<Parameter> GetXMLData2()
+            {
+                            
+                List<Parameter> Params = new List<Parameter>();
+                XmlDocument xDoc = new XmlDocument();
+                
+                xDoc.Load(path);
+                XmlNodeList ParametersDataSet = xDoc.SelectNodes("ParametersDataSet/Parameter");
+                foreach (XmlElement p in ParametersDataSet)
+                {
+                    Parameter x = new Parameter(p.SelectSingleNode("TimeSpan").InnerText);
+                    x.InterfaceName = p.SelectSingleNode("InterfaceName").InnerText;
+                    x.ArchiveDir = p.SelectSingleNode("ArchiveDir").InnerText;
+                    x.ProcesDir = p.SelectSingleNode("ProcesDir").InnerText;
+                    x.Retention = p.SelectSingleNode("Retention").InnerText;
+                    x.Recursive = Convert.ToBoolean(p.SelectSingleNode("Recursive").InnerText);
+                    x.Comments = p.SelectSingleNode("Comments").InnerText;
+ 
+
+                    if (null != p.SelectSingleNode("Include"))
+                    {
+                        x.Include = p.SelectSingleNode("Include").InnerText.ToString();
+                    }
+
+                    if (null != p.SelectSingleNode("Exclude"))
+                    {
+                       x.Exclude = p.SelectSingleNode("Exclude").InnerText.ToString();
+                    }
+                   
+                    Params.Add(x);
+                }
+                return Params;
+            }
+
+            private void WriteXMLData()
+            {
+                //Mooie output instellen
+                var settings = new XmlWriterSettings();
+                settings.OmitXmlDeclaration = true;
+                settings.Indent = true;
+                settings.NewLineOnAttributes = true;
+
+                using (XmlWriter writer = XmlWriter.Create(path, settings))
+                {
+                    //Wegschrijven naar XML
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("ParametersDataSet");
+                    for (int i = 0; i < Parameters.Count(); i++)
+                    {
+                        writer.WriteStartElement("Parameter");
+                        writer.WriteElementString("InterfaceName", Parameters[i].InterfaceName);
+                        writer.WriteElementString("ProcesDir", Parameters[i].ProcesDir);
+                        writer.WriteElementString("ArchiveDir", Parameters[i].ArchiveDir);
+                        switch (Parameters[i].Timespan)
+                        {
+                            case 0:
+                                writer.WriteElementString("TimeSpan", "D");
+                                break;
+                            case 1:
+                                writer.WriteElementString("TimeSpan", "W");
+                                break;
+                            case 2:
+                                writer.WriteElementString("TimeSpan", "M");
+                                break;
+                            case 3:
+                                writer.WriteElementString("TimeSpan", "Y");
+                                break;
+                            default:
+                                writer.WriteElementString("TimeSpan", "D");
+                                break;
+                        }
+                        writer.WriteElementString("Recursive", Parameters[i].Recursive.ToString());
+                        writer.WriteElementString("Retention", Parameters[i].Retention.ToString());
+
+                        if (Parameters[i].Include != null)
+                        {
+                            string[] include = Parameters[i].Include.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                            foreach (string x in include)
+                            {
+                                writer.WriteElementString("Include", x);
+                            }
+                        }
+
+                        if (Parameters[i].Exclude != null)
+                        {
+                            string[] exclude = Parameters[i].Exclude.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                            foreach (string x in exclude)
+                            {
+                                writer.WriteElementString("Exclude", x);
+                            }
+                        }
+                        writer.WriteElementString("Comments", Parameters[i].Comments);
+                        writer.WriteEndElement();
+                    }
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+            }
+        
 
         //Hier de functies voor het vullen van de velden
         //Leegmaken en vullen van het formulier
@@ -63,7 +278,7 @@ namespace ArchiveUI
             comboBoxInterfaceName.Text = Parameters[counter].InterfaceName;
             textBoxProcesDir.Text = Parameters[counter].ProcesDir;
             textBoxArchiveDir.Text = Parameters[counter].ArchiveDir;
-            comboBoxTimespan.SelectedIndex = Parameters[counter].Timespan; 
+            comboBoxTimespan.SelectedIndex = Convert.ToInt16(Parameters[counter].Timespan);
             checkBoxRecursive.Checked = Parameters[counter].Recursive;
             textBoxRetention.Text = Parameters[counter].Retention.ToString();
             textBoxInclude.Text = Parameters[counter].Include;
@@ -145,9 +360,10 @@ namespace ArchiveUI
                 counter = (Parameters.Count());
                 CleanupForm();             
                 ParameterNo.Text = (Parameters.Count() + 1).ToString();
-                Array.Resize(ref Parameters, counter + 1);
-                Parameters[counter] = new Parameter() { };
-                Parameters[counter].Timespan = 1;
+  
+
+                Parameters.Add(new Parameter("1") {});
+
                 FillComboBox();
                 ValidateForm();
                 Enable_edit = true;
@@ -160,14 +376,14 @@ namespace ArchiveUI
             CleanupForm();
             if (Parameters.Count() > 0) 
             { 
-                Parameters = Parameters.Where((source, index) => index != counter).ToArray();
+                 Parameters.RemoveAt(counter);
+
             }
             if (counter == Parameters.Count() && counter > 0) { counter--; }        
             if (Parameters.Count() > 0) { FillData(); }
             if (Parameters.Count() == 0)
             {
-                Array.Resize(ref Parameters, 1);
-                Parameters[0] = new Parameter() { };
+                Parameters.Add(new Parameter("0"));
             }
         }
 
@@ -185,7 +401,7 @@ namespace ArchiveUI
             //XML opnieuw inladen
             CleanupForm();
             counter = 0;
-            Parameters = GetXMLData();
+            Parameters = GetXMLData2();
             FillData();
         }
 
@@ -211,11 +427,12 @@ namespace ArchiveUI
                 Parameters[counter].Comments = textBoxComments.Text;
                 Parameters[counter].ProcesDir = textBoxProcesDir.Text;
                 Parameters[counter].ArchiveDir = textBoxArchiveDir.Text;
-                if (double.TryParse(textBoxRetention.Text, out num)) { Parameters[counter].Retention = Convert.ToInt16(textBoxRetention.Text); }
-                Parameters[counter].Recursive = checkBoxRecursive.Checked;
+               if (double.TryParse(textBoxRetention.Text, out num)) { Parameters[counter].Retention = Convert.ToInt16(textBoxRetention.Text).ToString(); }
+               Parameters[counter].Recursive = checkBoxRecursive.Checked;
                 Parameters[counter].Include = textBoxInclude.Text;
                 Parameters[counter].Exclude = textBoxExclude.Text;
                 Parameters[counter].Timespan = comboBoxTimespan.SelectedIndex;
+                Console.WriteLine(comboBoxTimespan.SelectedIndex.ToString());
             }  
             ValidateForm();         
         }
